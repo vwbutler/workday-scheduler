650ebed2728f68ba6002d316ae72aeefd2fc0677
Virginia Butler <VButler@masc.sc>
2023-05-12 09:26:52 -0400

    final updates

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# workday-scheduler
-Module 5 Challenge
+# 05 Third-Party APIs: Work Day Scheduler
+
+## Description
+
+A simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+This solution utilizes the [Day.js](https://day.js.org/en/) library to work with date and time. 
+
+## User Story
+
+```md
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Visual
+
+The following image shows a view of the workday calendar.
+
+![Work Day Scheduler](./assets/images/workdayscheduler.png)
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Deployed Application
+
+View the deployed application on Github at [https://vwbutler.github.io/workday-scheduler](https://vwbutler.github.io/workday-scheduler).
+
+## Need help?
+
+Contact [Virginia Butler](https://github.com/vwbutler) through Github.
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,52 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-  //$ is the selector$('.class'), $('#id')
-
   var todayFormatted = dayjs().format("MMM D, YYYY");
   $("#currentDay").text(todayFormatted);
+
+  var currentHour = dayjs().hour();
+
+  var allRows = document.querySelectorAll(".time-block");
+
+  $(".time-block").each(function (index) {
+    var timeVal = $(this).attr("id").split("-")[1];
+
+    if (timeVal > currentHour) {
+      $(this).addClass("future");
+    } else if (timeVal == currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("past");
+    }
+  });
+});
+//The Save button listens for the "click" and traverses the DOM to get the sibling within div id hour ## to display the text area.
+$(".saveBtn").each(function () {
+  $(this).on("click", function () {
+    var calendarItem = $(this).siblings().next().val();
+    console.log(calendarItem);
+
+    //The system saves the information into local storage and gets it from local storage
+    var calendarData = JSON.parse(localStorage.getItem("calendarData") || "{}");
+
+    var calendarHour = $(this).siblings().prev().text();
+
+    calendarData[calendarHour] = calendarItem;
+
+    localStorage.setItem("calendarData", JSON.stringify(calendarData));
+  });
 });
 
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+//Attempting to get data from local storage and display it on the page when the page loads.
+$(document).ready(function () {
+  console.log("page has loaded!");
 
-//grab the id of each number of each timeblock div and grab the number of from the id
+  var calendarData = JSON.parse(localStorage.getItem("calendarData") || "{}");
 
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+  //matching the data to the row and setting text
+  $(".time-block").each(function () {
+    var hour = $(this).attr("id").split("-")[1];
 
-//the only way to add functionality is with JavaScript
+    var calendarItem = calendarData[hour];
+
+    $(this).find("textarea").val(calendarItem);
+  });
+});
